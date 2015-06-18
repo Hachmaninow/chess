30a0baaa5a90d235962375558ca5fa4baf165f21
Hachmaninow <b.hachmann@gmail.com>
2015-06-18 14:50:21 +0200

    support search for castlings among valid moves

--- src/chess/board.clj
+++ src/chess/board.clj
@@ -35,10 +35,18 @@
 (def start-position
   (reduce place-piece empty-board (for [[piece squares] initial-squares square squares] [piece square])))
 
+(defn guess-castling-rights [board]
+  {:white (set (remove nil? [(when (and (= :K (lookup board :e1)) (= :R (lookup board :h1))) :0-0)
+                             (when (and (= :K (lookup board :e1)) (= :R (lookup board :a1))) :0-0-0)]))
+   :black (set (remove nil? [(when (and (= :k (lookup board :e8)) (= :r (lookup board :h8))) :0-0)
+                             (when (and (= :k (lookup board :e8)) (= :r (lookup board :a8))) :0-0-0)]))})
+
 (defn setup
   ([] (setup start-position))
-  ([board] {:board board :player-to-move :white})
-  ([board options] (conj {:board board :player-to-move :white} options)))
+  ([board] {:board board :player-to-move :white :castling-rights (guess-castling-rights board)})
+  ([board options] (conj (setup board) options)))
+
+(defn switch-player [game] (assoc game :player-to-move (opponent (game :player-to-move))))
 
 (def direction-steps {:N 8 :S -8 :W -1 :E 1 :NE 9 :SE -7 :SW -9 :NW 7})
 (def straight [:N :E :S :W])
@@ -63,38 +71,75 @@
           (take-while #(empty-square? board %) (direction-vector from-index max-reach direction))
           (take 1 (drop-while #(empty-square? board %) (direction-vector from-index max-reach direction))))))))
 
+(defn piece-type [piece]
+  "Returns the type of the piece represented by the keyword of the respective white piece irrelevant of the color."
+  (keyword (clojure.string/upper-case (subs (str piece) 1))))
 
+(defmulti attacked-indexes (fn [game idx] (piece-type (get-in game [:board idx]))))
 
-(defmulti candidate-indexes (fn [game idx] (keyword (clojure.string/upper-case (subs (str (get-in game [:board idx])) 1)))))
+(defmethod attacked-indexes :K [game idx] (reachable-indexes idx (game :board) 1 straight-and-diagonal))
 
-(defmethod candidate-indexes :K [game idx] (reachable-indexes idx (game :board) 1 straight-and-diagonal))
+(defmethod attacked-indexes :Q [game idx] (reachable-indexes idx (game :board) 7 straight-and-diagonal))
 
-(defmethod candidate-indexes :Q [game idx] (reachable-indexes idx (game :board) 7 straight-and-diagonal))
+(defmethod attacked-indexes :R [game idx] (reachable-indexes idx (game :board) 7 straight))
 
-(defmethod candidate-indexes :R [game idx] (reachable-indexes idx (game :board) 7 straight))
+(defmethod attacked-indexes :B [game idx] (reachable-indexes idx (game :board) 7 diagonal))
 
-(defmethod candidate-indexes :B [game idx] (reachable-indexes idx (game :board) 7 diagonal))
-
-(defmethod candidate-indexes :N [_ idx]
+(defmethod attacked-indexes :N [_ idx]
   (let [knight-moves [+17 +10 -6 -15 -17 -10 +6 +15]]
     (filter #(and (= (distance % idx) 2) (< % 64) (>= % 0)) (map #(+ idx %) knight-moves))))
 
-(defmethod candidate-indexes :P [game idx]
+(defmethod attacked-indexes :P [game idx]
   (let [board (game :board) color (game :player-to-move)
-        op (if (= :white color) + -) origin-rank (if (= :white color) 1 6)
-        s1 (op idx 8) s2 (op idx 16) s3 (op idx 7) s4 (op idx 9)]
+        op (if (= :white color) + -) s1 (op idx 7) s2 (op idx 9)]
     (vector
-      (when (empty-square? board s1) s1)                    ; single-step forward
-      (when (and (= (rank idx) origin-rank) (empty-square? board s1) (empty-square? board s2)) s2) ; double-step forward
-      (when (and (= (distance idx s3) 1) (= (piece-color (get board s3)) (opponent color))) s3) ; take
-      (when (and (= (distance idx s4) 1) (= (piece-color (get board s4)) (opponent color))) s4))))
+      (when (and (= (distance idx s1) 1) (= (piece-color (get board s1)) (opponent color))) s1)
+      (when (and (= (distance idx s2) 1) (= (piece-color (get board s2)) (opponent color))) s2))))
 
-(defn find-moves-for-index [game occupations idx]
-  (let [candidates (remove nil? (remove occupations (candidate-indexes game idx)))]
-    (map #(into {} {:from idx :to %}) candidates)))
+(defn all-attacked-indexes [game]
+  (set (mapcat #(attacked-indexes game %) (occupied-indexes (game :board) (game :player-to-move)))))
+
+(defn find-attacking-moves-for-index [game occupations idx]
+  (let [candidates (remove occupations (attacked-indexes game idx))]
+    (map #(when % {:move [idx %]}) candidates)))
+
+(defn find-forward-pawn-moves [game idx]
+  (let [board (game :board) color (game :player-to-move)
+        op (if (= :white color) + -) origin-rank (if (= :white color) 1 6) s1 (op idx 8) s2 (op idx 16)]
+    (vector
+      (when (empty-square? board s1) {:move [idx s1]})      ; single-step forward
+      (when (and (= (rank idx) origin-rank) (empty-square? board s1) (empty-square? board s2)) {:move [idx s2]})))) ; double-step forward
+
+(def castlings
+  {:white {:0-0   {:king-move [(to-index :e1) (to-index :g1)] :rook-move [(to-index :h1) (to-index :f1)] :transfer-indexes (map to-index [:e1 :f1 :g1])}
+           :0-0-0 {:king-move [(to-index :e1) (to-index :c1)] :rook-move [(to-index :a1) (to-index :d1)] :transfer-indexes (map to-index [:e1 :d1 :c1 :b1])}}
+   :black {:0-0   {:king-move [(to-index :e8) (to-index :g8)] :rook-move [(to-index :h8) (to-index :f8)] :transfer-indexes (map to-index [:e8 :f8 :g8])}
+           :0-0-0 {:king-move [(to-index :e8) (to-index :c8)] :rook-move [(to-index :a8) (to-index :d8)] :transfer-indexes (map to-index [:e8 :d8 :c8 :b8])}}})
+
+(defn check-castling [game attacked-indexes-by-opponent [castling-type rules]]
+  (when (and
+          (get-in game [:castling-rights (game :player-to-move) castling-type])
+          (empty? (clojure.set/intersection attacked-indexes-by-opponent (set (take 3 (rules :transfer-indexes))))) ; only three transfer indexes must not be attacked by opponent
+          (every? (partial empty-square? (game :board)) (rest (rules :transfer-indexes)))) ; only last two indexes must be empty
+    {:move (get rules :king-move) :secondary-move (get rules :rook-move) :special-move castling-type}))
+
+(defn find-castlings [game]
+  (let [player (game :player-to-move) castlings (castlings player)
+        attacked-indexes-by-opponent (all-attacked-indexes (switch-player game))]
+    (map (partial check-castling game attacked-indexes-by-opponent) castlings)))
+
+(defn find-moves [game]
+  (let [board (game :board) occupations (occupied-indexes board (game :player-to-move))]
+    (remove nil?
+            (flatten
+              (concat
+                (map #(find-attacking-moves-for-index game occupations %) occupations)
+                (map #(when (or (= (get board %) :p) (= (get board %) :P)) (find-forward-pawn-moves game %)) occupations)
+                (find-castlings game)
+                )))))
+;
 
 (defn valid-moves [game]
-  (let [occupations (occupied-indexes (game :board) (game :player-to-move))]
-    (mapcat #(find-moves-for-index game occupations %) occupations)))
+  (find-moves game))
 
 ;  (use 'chess.board)
--- test/chess/board_test.clj
+++ test/chess/board_test.clj
@@ -4,12 +4,9 @@
             [chess.fen :refer :all]))
 
 (deftest test-piece-color
-  (testing "white pieces"
-    (is (= :white (piece-color :P) (piece-color :B) (piece-color :N) (piece-color :R) (piece-color :Q) (piece-color :K))))
-  (testing "black pieces"
-    (is (= :black (piece-color :p) (piece-color :b) (piece-color :n) (piece-color :r) (piece-color :q) (piece-color :k))))
-  (testing "empty squares"
-    (is (nil? (piece-color nil)))))
+  (is (= :white (piece-color :P) (piece-color :B) (piece-color :N) (piece-color :R) (piece-color :Q) (piece-color :K)))
+  (is (= :black (piece-color :p) (piece-color :b) (piece-color :n) (piece-color :r) (piece-color :q) (piece-color :k)))
+  (is (nil? (piece-color nil))))
 
 (deftest test-opponent
   (is (= :black (opponent :white)))
@@ -65,6 +62,19 @@
 (deftest test-start-position
   (is (= [:R :N :B :Q :K :B :N :R :P :P :P :P :P :P :P :P nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil :p :p :p :p :p :p :p :p :r :n :b :q :k :b :n :r] start-position)))
 
+(deftest test-guess-castling-rights
+  (is (= {:white #{:0-0 :0-0-0} :black #{:0-0 :0-0-0}} (guess-castling-rights start-position)))
+  (is (= {:white #{:0-0-0} :black #{:0-0}} (guess-castling-rights (place-pieces empty-board [:K :e1 :R :a1 :k :e8 :r :h8]))))
+  (is (= {:white #{} :black #{}} (guess-castling-rights (place-pieces empty-board [:K :e2 :R :a1])))))
+
+(deftest test-setup
+  (is (= start-position ((setup) :board)))
+  (is (= :white ((setup) :player-to-move))))
+
+(deftest test-switch-player
+  (is (= :black ((switch-player (setup)) :player-to-move)))
+  (is (= :white ((switch-player (switch-player (setup))) :player-to-move))))
+
 (defn direction-square-vector [square direction]
   (map to-square (direction-vector (to-index square) 7 direction)))
 
@@ -118,49 +128,75 @@
   (testing "non-empty square"
     (is (false? (empty-square? start-position 58)))))
 
-(defn squares-in-reach
-  ([piece square] (squares-in-reach piece square nil))
+(defn target-squares
+  "Find all valid target squares for the specified piece being located on the specified square on
+  a board together with the specified additional pieces."
+  ([piece square] (target-squares piece square nil))
   ([piece square additional-pieces]
-   (let [board-with-moving-piece (place-piece empty-board [piece square])
-         board-with-pieces (place-pieces board-with-moving-piece additional-pieces)
-         game (setup board-with-pieces {:player-to-move (piece-color piece)})]
-     (set (map #(to-square (% :to)) (filter #(= (% :from) (to-index square)) (valid-moves game)))))))
+   (let [board (place-pieces (place-piece empty-board [piece square]) additional-pieces)
+         game (setup board {:player-to-move (piece-color piece)})
+         all-valid-moves (valid-moves game)
+         moves-from-idx (filter #(= (first (% :move)) (to-index square)) all-valid-moves)]
+     (set (map #(to-square (last (% :move))) moves-from-idx)))))
 
-(deftest test-find-moves-on-empty-board
+(deftest test-attacking-moves-on-empty-board
   (testing "king on empty board"
-    (is (= #{:d5 :e4 :d3 :c4 :e5 :e3 :c3 :c5} (squares-in-reach :K :d4)))
-    (is (= #{:a2 :b1 :b2} (squares-in-reach :k :a1))))
+    (is (= #{:d5 :e4 :d3 :c4 :e5 :e3 :c3 :c5} (target-squares :K :d4)))
+    (is (= #{:a2 :b1 :b2} (target-squares :k :a1))))
   (testing "queen on empty board"
     (is (= #{:d5 :d6 :d7 :d8 :d3 :d2 :d1 :c4 :b4 :a4 :e4 :f4 :g4 :h4
-             :e5 :f6 :g7 :h8 :e3 :f2 :g1 :c3 :b2 :a1 :c5 :b6 :a7} (squares-in-reach :Q :d4)))
+             :e5 :f6 :g7 :h8 :e3 :f2 :g1 :c3 :b2 :a1 :c5 :b6 :a7} (target-squares :Q :d4)))
     (is (= #{:a2 :a3 :a4 :a5 :a6 :a7 :a8 :b1 :c1 :d1 :e1 :f1 :g1 :h1
-             :b2 :c3 :d4 :e5 :f6 :g7 :h8} (squares-in-reach :q :a1))))
+             :b2 :c3 :d4 :e5 :f6 :g7 :h8} (target-squares :q :a1))))
   (testing "rook on empty board"
-    (is (= #{:d5 :d6 :d7 :d8 :d3 :d2 :d1 :c4 :b4 :a4 :e4 :f4 :g4 :h4} (squares-in-reach :R :d4)))
-    (is (= #{:a2 :a3 :a4 :a5 :a6 :a7 :a8 :b1 :c1 :d1 :e1 :f1 :g1 :h1} (squares-in-reach :r :a1))))
+    (is (= #{:d5 :d6 :d7 :d8 :d3 :d2 :d1 :c4 :b4 :a4 :e4 :f4 :g4 :h4} (target-squares :R :d4)))
+    (is (= #{:a2 :a3 :a4 :a5 :a6 :a7 :a8 :b1 :c1 :d1 :e1 :f1 :g1 :h1} (target-squares :r :a1))))
   (testing "bishop on empty board"
-    (is (= #{:e5 :f6 :g7 :h8 :e3 :f2 :g1 :c3 :b2 :a1 :c5 :b6 :a7} (squares-in-reach :B :d4)))
-    (is (= #{:b2 :c3 :d4 :e5 :f6 :g7 :h8} (squares-in-reach :b :a1))))
+    (is (= #{:e5 :f6 :g7 :h8 :e3 :f2 :g1 :c3 :b2 :a1 :c5 :b6 :a7} (target-squares :B :d4)))
+    (is (= #{:b2 :c3 :d4 :e5 :f6 :g7 :h8} (target-squares :b :a1))))
   (testing "knight on empty board"
-    (is (= #{:f6 :g5 :g3 :f2 :d2 :c3 :c5 :d6} (squares-in-reach :n :e4)))
-    (is (= #{:g6 :f7} (squares-in-reach :N :h8))))
+    (is (= #{:f6 :g5 :g3 :f2 :d2 :c3 :c5 :d6} (target-squares :n :e4)))
+    (is (= #{:g6 :f7} (target-squares :N :h8))))
   (testing "pawn on empty board"
-    (is (= #{:a5} (squares-in-reach :P :a4)))
-    (is (= #{:e3 :e4} (squares-in-reach :P :e2)))
-    (is (= #{:d1} (squares-in-reach :p :d2)))
-    (is (= #{:g6 :g5} (squares-in-reach :p :g7)))))
+    (is (= #{:a5} (target-squares :P :a4)))
+    (is (= #{:e3 :e4} (target-squares :P :e2)))
+    (is (= #{:d1} (target-squares :p :d2)))
+    (is (= #{:g6 :g5} (target-squares :p :g7)))))
 
-(deftest test-find-moves-on-non-empty-board
+(deftest test-valid-moves-on-non-empty-board
   (testing "king on non-empty board"
-    (is (= #{:d5 :d3 :c4 :e5 :e3 :c3} (squares-in-reach :K :d4 [:Q :e4 :R :c5 :b :d3]))))
+    (is (= #{:d5 :d3 :c4 :e5 :e3 :c3} (target-squares :K :d4 [:Q :e4 :R :c5 :b :d3])))
+    (is (= #{:d1 :d2 :e2 :f2 :f1 :g1 :c1} (target-squares :K :e1 [:R :a1 :R :h1]))))
   (testing "queen on non-empty board"
-    (is (= #{:b1 :c1 :d1 :e1} (squares-in-reach :Q :a1 [:B :b2 :K :a2 :r :e1]))))
+    (is (= #{:b1 :c1 :d1 :e1} (target-squares :Q :a1 [:B :b2 :K :a2 :r :e1]))))
   (testing "rook on non-empty board"
-    (is (= #{:d5 :d6 :d7 :d8 :c4 :b4 :a4 :e4 :f4} (squares-in-reach :R :d4 [:B :d3 :q :f4]))))
+    (is (= #{:d5 :d6 :d7 :d8 :c4 :b4 :a4 :e4 :f4} (target-squares :R :d4 [:B :d3 :q :f4]))))
   (testing "bishop on non-empty board"
-    (is (= #{:e5 :f6 :g7 :h8 :e3 :f2 :g1 :c5 :b6} (squares-in-reach :B :d4 [:K :c3 :r :b6]))))
+    (is (= #{:e5 :f6 :g7 :h8 :e3 :f2 :g1 :c5 :b6} (target-squares :B :d4 [:K :c3 :r :b6]))))
   (testing "knight on non-empty board"
-    (is (= #{:e2 :f5 :e6 :c2 :f3 :b3 :b5} (squares-in-reach :N :d4 [:K :e3 :R :c6 :q :b3]))))
+    (is (= #{:e2 :f5 :e6 :c2 :f3 :b3 :b5} (target-squares :N :d4 [:K :e3 :R :c6 :q :b3]))))
   (testing "pawn on non-empty board"
-    (is (= #{:a5 :b5} (squares-in-reach :P :a4 [:p :h5 :b :b5])))
-    (is (= #{:d6 :f6} (squares-in-reach :p :e7 [:N :e6 :Q :f6 :R :d6])))))
+    (is (= #{:a5 :b5} (target-squares :P :a4 [:p :h5 :b :b5])))
+    (is (= #{:d6 :f6} (target-squares :p :e7 [:N :e6 :Q :f6 :R :d6])))))
+
+(deftest test-find-castlings
+  (is (= '(nil nil) (find-castlings (setup))))
+  (is (= '({:move [4 6] :secondary-move [7 5] :special-move :0-0} {:move [4 2] :secondary-move [0 3] :special-move :0-0-0})
+         (find-castlings (setup (place-pieces empty-board [:K :e1 :R :a1 :R :h1])))))
+  (is (= '(nil {:move [60 58] :secondary-move [56 59] :special-move :0-0-0})
+         (find-castlings (switch-player (setup (place-pieces empty-board [:k :e8 :r :a8]))))))
+  (is (= '(nil nil)
+         (find-castlings (setup (place-pieces empty-board [:K :e1 :R :a1 :R :h1 :b :e3])))))
+  (is (= '({:move [4 6] :secondary-move [7 5] :special-move :0-0} {:move [4 2] :secondary-move [0 3] :special-move :0-0-0})
+         (find-castlings (setup (place-pieces empty-board [:K :e1 :R :a1 :R :h1 :b :e4])))))
+  (is (= '(nil nil)
+         (find-castlings (setup (place-pieces empty-board [:K :e1 :R :a1 :R :h1 :r :e8])))))
+  (is (= '(nil nil)
+         (find-castlings (setup (place-pieces empty-board [:K :e1 :R :a1 :R :h1 :n :b1 :n :g1])))))
+  )
+;
+;(deftest test-attacks
+;  (testing "attacks on empty board"
+;    (is (= true (attacks? (setup (place-piece empty-board [:R :d4])) :white (to-index :h4))))
+;    (is (= true (attacks? (setup (place-piece empty-board [:b :d4])) :black (to-index :a1))))
+;    (is (= false (attacks? (setup (place-piece empty-board [:Q :d4])) :white (to-index :h5))))))
