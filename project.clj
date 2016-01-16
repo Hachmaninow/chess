a9d1620e63e26a4e4f048f32b8094ca2a3d1e396
Hachmaninow <b.hachmann@gmail.com>
2016-01-16 14:55:31 +0100

    rename to chess-dojo

--- project.clj
+++ project.clj
@@ -1,24 +1,123 @@
-(defproject chess "0.1.0-SNAPSHOT"
+(defproject chess-dojo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-                 [org.clojure/clojure "1.6.0"]
+  :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+
                  [instaparse "1.4.1"]
                  [spyscope "0.1.5"]
-                 [criterium "0.4.3"]
                  [com.taoensso/timbre "4.1.4"]
-                 [ring/ring-core "1.4.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring-server "0.4.0"]
+
+                 [reagent "0.5.1" :exclusions [org.clojure/tools.reader]]
+                 [reagent-forms "0.5.13"]
+                 [reagent-utils "0.1.7"]
+                 [ring "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
                  [compojure "1.4.0"]
-                 [enlive "1.1.6"]
-                 [me.raynes/fs "1.4.6"]
+                 [hiccup "1.0.5"]
+                 [environ "1.0.1"]
+                 [secretary "1.2.3"]
+                 [venantius/accountant "0.1.6" :exclusions [org.clojure/tools.reader]]
                  ]
-  :plugins [[lein-ring "0.9.7"]]
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}}
-  :ring {:handler chess.web/app}
-  :source-paths ["src/clj"]
-  :test-paths ["test/clj"]
-  )
+
+  :plugins [[lein-environ "1.0.1"]
+            [lein-cljsbuild "1.1.2"]
+            [lein-asset-minifier "0.2.4" :exclusions [org.clojure/clojure]]
+            [lein-doo "0.1.6"]
+            ]
+
+  :ring {:handler chess-dojo.handler/app :uberwar-name "chess-dojo.war"}
+
+  :min-lein-version "2.5.0"
+
+  :uberjar-name "chess-dojo.jar"
+
+  :main chess-dojo.server
+
+  :clean-targets ^{:protect false} [:target-path
+                                    [:cljsbuild :builds :app :compiler :output-dir]
+                                    [:cljsbuild :builds :app :compiler :output-to]]
+
+  :source-paths ["src/main/clj" "src/main/cljc"]
+  :test-paths ["src/test/clj" "src/test/cljc"]
+  :resource-paths ["resources" "target/cljsbuild"]
+
+  :minify-assets {:assets {"resources/public/css/site.min.css" "resources/public/css/site.css"}}
+
+  :cljsbuild {:builds {:app {:source-paths ["src/main/cljs" "src/main/cljc"]
+                             :compiler {:output-to "target/cljsbuild/public/js/app.js"
+                                        :output-dir "target/cljsbuild/public/js/out"
+                                        :asset-path "js/out"
+                                        :optimizations :none
+                                        :pretty-print true}}
+                       :browser-test {:source-paths ["src/main/cljs" "src/main/cljc" "src/test/cljs" "src/test/cljc"]
+                                      :compiler {:output-to "target/browser_tests.js"
+                                                 :main 'chess-dojo.browser-test
+                                                 :optimizations :none}}}}
+
+  :doo {:paths {:karma "~/node_modules/karma/bin/karma"}}
+
+
+  :profiles {:dev {:repl-options {:init-ns chess-dojo.repl}
+                   :dependencies [[ring/ring-mock "0.3.0"]
+                                  [ring/ring-devel "1.4.0"]
+                                  [prone "0.8.3"]
+                                  [lein-figwheel "0.5.0-2" :exclusions [org.clojure/core.memoize
+                                                                        ring/ring-core
+                                                                        org.clojure/clojure
+                                                                        org.ow2.asm/asm-all
+                                                                        org.clojure/data.priority-map
+                                                                        org.clojure/tools.reader
+                                                                        org.clojure/clojurescript
+                                                                        org.clojure/core.async
+                                                                        org.clojure/tools.analyzer.jvm]]
+                                  [org.clojure/clojurescript "1.7.228" :exclusions [org.clojure/clojure org.clojure/tools.reader]]
+                                  [org.clojure/tools.nrepl "0.2.12"]
+                                  [com.cemerick/piggieback "0.2.1"]
+                                  [pjstadig/humane-test-output "0.7.1"]
+                                  ]
+
+                   :source-paths ["env/dev/clj"]
+                   :plugins [[lein-figwheel "0.5.0-2" :exclusions [org.clojure/core.memoize
+                                                                   ring/ring-core
+                                                                   org.clojure/clojure
+                                                                   org.ow2.asm/asm-all
+                                                                   org.clojure/data.priority-map
+                                                                   org.clojure/tools.reader
+                                                                   org.clojure/clojurescript
+                                                                   org.clojure/core.async
+                                                                   org.clojure/tools.analyzer.jvm]]
+                             [org.clojure/clojurescript "1.7.228"]
+                             ]
+
+                   :injections [(require 'pjstadig.humane-test-output)
+                                (pjstadig.humane-test-output/activate!)]
+
+                   :figwheel {:http-server-root "public"
+                              :server-port 3449
+                              :nrepl-port 7002
+                              :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"]
+                              :css-dirs ["resources/public/css"]
+                              :ring-handler chess-dojo.handler/app}
+
+                   :env {:dev true}
+
+                   :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]
+                                              :compiler {:main "chess-dojo.dev"
+                                                         :source-map true}}}}}
+
+             :uberjar {:hooks [minify-assets.plugin/hooks]
+                       :source-paths ["env/prod/clj"]
+                       :prep-tasks ["compile" ["cljsbuild" "once"]]
+                       :env {:production true}
+                       :aot :all
+                       :omit-source true
+                       :cljsbuild {:jar true
+                                   :builds {:app
+                                            {:source-paths ["env/prod/cljs"]
+                                             :compiler
+                                             {:optimizations :advanced
+                                              :pretty-print false}}}}}})
